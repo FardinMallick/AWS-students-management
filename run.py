e16cefbf00d2701070f8c55a8e9476a177753de1
Fardin Nesar <fardinmalick112@gmail.com>
2026-05-28 22:07:47 +0530

    Fixed Render porting blinding issue

--- run.py
+++ run.py
@@ -1,26 +1,46 @@
+import os
+
 from app import create_app
 from app import db
 
 from app.models.student import Student
 
 
-# Create Flask App
+# =========================
+# CREATE FLASK APP
+# =========================
 
 app = create_app()
 
 
-# Create Database Tables
+# =========================
+# CREATE DATABASE TABLES
+# =========================
 
 with app.app_context():
 
     db.create_all()
 
 
-# Run Application
+# =========================
+# RUN APPLICATION
+# =========================
 
 if __name__ == "__main__":
 
-    app.run(
-        debug=True
+    port = int(
+
+        os.environ.get(
+            "PORT",
+            5000
+        )
     )
 
+    app.run(
+
+        host="0.0.0.0",
+
+        port=port,
+
+        debug=True
+    )
